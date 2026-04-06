29b61064fc4fe288dc2e2bcfe2ccdf0d4783df7d
amell28 <siti24si@mahasiswa.pcr.ac.id>
2026-04-06 08:09:59 +0700

    Pengiriman data intent

---
+++ app/src/main/java/com/example/amelapps/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,28 @@
+package com.example.amelapps.pertemuan_4
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.amelapps.R
+
+class FourthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fourth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val name = intent.getStringExtra("nama")
+        val from = intent.getStringExtra("asal")
+        val age = intent.getIntExtra("umur",0)
+
+        Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+    }
+}
